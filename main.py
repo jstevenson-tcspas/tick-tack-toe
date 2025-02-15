09465c463d12e845dd6ed16f0406459e5a8d08ad
dpanjanon-tcspas <daikipanjanon@gmail.com>
2025-02-15 15:54:10 -0800

    Updated tic tac toe

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 print ('''
 ------------   
 TIC TACK TOE
@@ -7,7 +8,7 @@ p1 = ""
 p2 = ""
 # list- list of values (variables, integers, strings, etc.)
 
-board = ["", "", "", "", "", "", "", "", ""]
+board = ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"]
 # index: 0    1   2   3   4   5   6   7   8
 
 choice = input("X or 0? >")
@@ -19,12 +20,25 @@ if choice == "X":
 Team X 
 ------
     ''')
-elif choice == "0":
+else:
     p1 = "0"
     p2 = "X"
-    ('''
+    print('''
 ------
 Team 0
 ------
     ''')
-print("------------------------")
+
+def grid():
+    print("------------------------")
+    print(" ".join(board[0:3]))
+    print(" ".join(board[3:6]))
+    print(" ".join(board[6:9]))
+    print("------------------------")
+
+    
+grid()
+
+
+pos = input("where do you want to place it?\n>")
+if pos == 
